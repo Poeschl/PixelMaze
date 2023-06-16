dde51f8c5361c7116601a46efdd6a453c4ebbff0
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-06-16 11:36:48 +0200

    :art: Reformat code to short intents

--- src/main/kotlin/xyz/poeschl/pixelmaze/Maze.kt
+++ src/main/kotlin/xyz/poeschl/pixelmaze/Maze.kt
@@ -15,98 +15,98 @@ import kotlin.math.max
 import kotlin.math.min
 
 class Maze(
-    private val origin: Point,
-    private val mazeSize: Pair<Int, Int>,
-    private val pathSize: Int
+  private val origin: Point,
+  private val mazeSize: Pair<Int, Int>,
+  private val pathSize: Int
 ) {
 
-    companion object {
-        private val VISUAL_LOGGER = KotlinLogging.logger("visualDebug")
-        private val DEBUG = VISUAL_LOGGER.isDebugEnabled
-        private const val BORDER_WIDTH = 1
-        private val WALL_COLOR = Color.WHITE
+  companion object {
+    private val VISUAL_LOGGER = KotlinLogging.logger("visualDebug")
+    private val DEBUG = VISUAL_LOGGER.isDebugEnabled
+    private const val BORDER_WIDTH = 1
+    private val WALL_COLOR = Color.WHITE
+  }
+
+  private val cellSize = pathSize + BORDER_WIDTH * 2
+  val widthInCells = mazeSize.first / cellSize
+  val heightInCells = mazeSize.second / cellSize
+
+  private var shadowMatrix = PixelMatrix(mazeSize.first, mazeSize.second)
+  var mazeSet = setOf<Pixel>()
+
+  fun updateMaze(edges: Stream<Edge>) {
+    createGrid()
+    edges.parallel().forEach { createEdges(it) }
+    mazeSet = shadowMatrix.getPixelSet().map { Pixel(it.point.plus(origin), it.color) }.toSet()
+    shadowMatrix = PixelMatrix(mazeSize.first, mazeSize.second)
+  }
+
+  fun clear() {
+    mazeSet = setOf()
+  }
+
+  private fun createGrid() {
+    runBlocking {
+      launch {
+        IntStream.rangeClosed(0, widthInCells)
+          .mapToObj { x ->
+            createVerticalPixels(
+              Point(x * cellSize, 0),
+              heightInCells * cellSize,
+              WALL_COLOR
+            )
+          }
+          .flatMap { it.parallelStream() }
+          .forEach { shadowMatrix.insert(it) }
+      }
+      launch {
+        IntStream.rangeClosed(0, heightInCells)
+          .mapToObj { y ->
+            createHorizontalPixels(
+              Point(0, y * cellSize),
+              widthInCells * cellSize,
+              WALL_COLOR
+            )
+          }
+          .flatMap { it.parallelStream() }
+          .forEach { shadowMatrix.insert(it) }
+      }
     }
+  }
 
-    private val cellSize = pathSize + BORDER_WIDTH * 2
-    val widthInCells = mazeSize.first / cellSize
-    val heightInCells = mazeSize.second / cellSize
+  private fun createEdges(edge: Edge) {
+    val from = getOriginPointOfCell(min(edge.either(), edge.other()))
+    val to = getOriginPointOfCell(max(edge.either(), edge.other()))
 
-    private var shadowMatrix = PixelMatrix(mazeSize.first, mazeSize.second)
-    var mazeSet = setOf<Pixel>()
-
-    fun updateMaze(edges: Stream<Edge>) {
-        createGrid()
-        edges.parallel().forEach { createEdges(it) }
-        mazeSet = shadowMatrix.getPixelSet().map { Pixel(it.point.plus(origin), it.color) }.toSet()
-        shadowMatrix = PixelMatrix(mazeSize.first, mazeSize.second)
+    when {
+      from.x == to.x && from.y != to.y -> removeHorizontalBorderToRightOf(from)
+      from.y == to.y && from.x != to.x -> removeVerticalBorderToBottomOf(from)
     }
+  }
 
-    fun clear() {
-        mazeSet = setOf()
+  private fun removeVerticalBorderToBottomOf(from: Point) {
+    for (yOffset in 0..pathSize) {
+      val wallPoint = from.plus(Point(cellSize, BORDER_WIDTH + yOffset))
+      shadowMatrix.remove(wallPoint)
+      if (DEBUG) {
+        shadowMatrix.insert(Pixel(wallPoint, Color.RED))
+      }
     }
+  }
 
-    private fun createGrid() {
-        runBlocking {
-            launch {
-                IntStream.rangeClosed(0, widthInCells)
-                    .mapToObj { x ->
-                        createVerticalPixels(
-                            Point(x * cellSize, 0),
-                            heightInCells * cellSize,
-                            WALL_COLOR
-                        )
-                    }
-                    .flatMap { it.parallelStream() }
-                    .forEach { shadowMatrix.insert(it) }
-            }
-            launch {
-                IntStream.rangeClosed(0, heightInCells)
-                    .mapToObj { y ->
-                        createHorizontalPixels(
-                            Point(0, y * cellSize),
-                            widthInCells * cellSize,
-                            WALL_COLOR
-                        )
-                    }
-                    .flatMap { it.parallelStream() }
-                    .forEach { shadowMatrix.insert(it) }
-            }
-        }
+  private fun removeHorizontalBorderToRightOf(from: Point) {
+    for (xOffset in 0..pathSize) {
+      val wallPoint = from.plus(Point(BORDER_WIDTH + xOffset, cellSize))
+      shadowMatrix.remove(wallPoint)
+      if (DEBUG) {
+        shadowMatrix.insert(Pixel(wallPoint, Color.RED))
+      }
     }
+  }
 
-    private fun createEdges(edge: Edge) {
-        val from = getOriginPointOfCell(min(edge.either(), edge.other()))
-        val to = getOriginPointOfCell(max(edge.either(), edge.other()))
-
-        when {
-            from.x == to.x && from.y != to.y -> removeHorizontalBorderToRightOf(from)
-            from.y == to.y && from.x != to.x -> removeVerticalBorderToBottomOf(from)
-        }
-    }
-
-    private fun removeVerticalBorderToBottomOf(from: Point) {
-        for (yOffset in 0..pathSize) {
-            val wallPoint = from.plus(Point(cellSize, BORDER_WIDTH + yOffset))
-            shadowMatrix.remove(wallPoint)
-            if (DEBUG) {
-                shadowMatrix.insert(Pixel(wallPoint, Color.RED))
-            }
-        }
-    }
-
-    private fun removeHorizontalBorderToRightOf(from: Point) {
-        for (xOffset in 0..pathSize) {
-            val wallPoint = from.plus(Point(BORDER_WIDTH + xOffset, cellSize))
-            shadowMatrix.remove(wallPoint)
-            if (DEBUG) {
-                shadowMatrix.insert(Pixel(wallPoint, Color.RED))
-            }
-        }
-    }
-
-    private fun getOriginPointOfCell(index: Int): Point {
-        val y = (index / widthInCells) * cellSize
-        val x = (index % widthInCells) * cellSize
-        return Point(x, y)
-    }
+  private fun getOriginPointOfCell(index: Int): Point {
+    val y = (index / widthInCells) * cellSize
+    val x = (index % widthInCells) * cellSize
+    return Point(x, y)
+  }
 }
--- src/main/kotlin/xyz/poeschl/pixelmaze/PixelMatrix.kt
+++ src/main/kotlin/xyz/poeschl/pixelmaze/PixelMatrix.kt
@@ -5,22 +5,22 @@ import xyz.poeschl.kixelflut.Point
 
 class PixelMatrix(private val xSize: Int, private val ySize: Int) {
 
-    private val dataArray = initDataArray()
+  private val dataArray = initDataArray()
 
-    fun insert(pixel: Pixel) {
-        val coord = pixel.point
-        dataArray[coord.y][coord.x] = pixel
-    }
+  fun insert(pixel: Pixel) {
+    val coord = pixel.point
+    dataArray[coord.y][coord.x] = pixel
+  }
 
-    fun remove(point: Point) {
-        dataArray[point.y][point.x] = null
-    }
+  fun remove(point: Point) {
+    dataArray[point.y][point.x] = null
+  }
 
-    fun getPixelSet(): Set<Pixel> {
-        return dataArray.flatten().filter { it != null }.map { it!! }.toSet()
-    }
+  fun getPixelSet(): Set<Pixel> {
+    return dataArray.flatten().filter { it != null }.map { it!! }.toSet()
+  }
 
-    private fun initDataArray(): Array<Array<Pixel?>> {
-        return Array<Array<Pixel?>>(ySize + 1) { Array(xSize + 1) { null } }
-    }
+  private fun initDataArray(): Array<Array<Pixel?>> {
+    return Array<Array<Pixel?>>(ySize + 1) { Array(xSize + 1) { null } }
+  }
 }
